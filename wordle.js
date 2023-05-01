6f315f377577159129d8ff961b04f37d49c62d62
chiarab22 <cbocc@myuax.com>
2023-05-01 14:48:35 +0200

    wordle

---
+++ wordle.js
@@ -0,0 +1,63 @@
+//Creamos un array de palabras para que el juego elija una al azar
+const words = ["perro", "gato", "casa", "auto", "manzana", "libro", "arbol", "jardin","ironhack", "computadora", "naranja", "ciudad", "estudio", "familia", "amigo", "trabajo", "universidad"];
+
+const word = words[Math.floor(Math.random() * words.length)];
+
+// Creamos un array de la misma longitud que la palabra elegida, pero con guiones bajos
+const wordArray = Array(word.length).fill("_");
+
+let attemptsLeft = 6;
+
+//Recogemos los elementos del DOM
+const wordElement = document.getElementById("word");
+const guessElement = document.getElementById("guess");
+const submitButton = document.getElementById("submit");
+const outputElement = document.getElementById("output");
+
+wordElement.textContent = wordArray.join(" ");
+
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault(); 
+
+  const guess = guessElement.value.toLowerCase(); 
+
+  
+  if (guess.length !== word.length) {
+    outputElement.textContent = "La palabra debe tener " + word.length + " letras.";
+
+    if (attemptsLeft === 1) {
+      setTimeout(() => {
+        alert("No tienes más intentos. La palabra secreta era: " + word);
+      }, 1000);
+    }
+    
+    attemptsLeft--;
+  } else {
+
+    let correctLetters = 0;
+    for (let i = 0; i < word.length; i++) {
+      if (guess[i] === word[i]) {
+        wordArray[i] = guess[i];
+        correctLetters++;
+      }
+    }
+
+    wordElement.textContent = wordArray.join(" ");
+
+    if (correctLetters === word.length) {
+      outputElement.textContent = "Ha ganado! La palabra era: " + word + ".";
+    } else {
+      outputElement.textContent = "Casi aciertas. Tienes " + (attemptsLeft - 1) + " intentos.";
+      
+      if (attemptsLeft === 1) {
+        setTimeout(() => {
+          alert("No tienes más intentos. La palabra secreta era: " + word);
+        }, 1000);
+      }
+      
+      attemptsLeft--;
+    }
+  }
+  
+  guessElement.value = "";
+});
